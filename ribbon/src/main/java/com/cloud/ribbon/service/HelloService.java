db0779777746e987f08510a3fa90dcc3705251f1
buliwen <mr_blw163@163.com>
2018-11-07 10:58:42 +0800

    add: ribbon Hystrix断路器测试

--- ribbon/src/main/java/com/cloud/ribbon/service/HelloService.java
+++ ribbon/src/main/java/com/cloud/ribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.cloud.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,17 @@ public class HelloService {
     @Autowired
     RestTemplate template;
 
+    /**
+     * Hystrix :
+     *  当 service-hi 工程不可用的时候，service-ribbon调用 service-hi的API接口时，
+     *  会执行快速失败，直接返回一组字符串，而不是等待响应超时，这很好的控制了容器的线程阻塞。
+     */
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return template.getForObject("http://SERVICE-HI/hi?name=" + name,String.class);
+    }
+
+    public String hiError(String name){
+        return "hi,"+name+",sorry,error!";
     }
 }
